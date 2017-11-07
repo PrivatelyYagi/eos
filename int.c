9c62eb3100e65fddb5d57ead8603649988c83925
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-11-07 19:57:07 +0900

    aa

--- int.c
+++ int.c
@@ -2,37 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+
+#define GPIOPATH "/sys/class/gpio/"
+#define GPIO2 GPIOPATH "gpio2/value"
+#define GPIO3 GPIOPATH "gpio3/value"
+#define GPIO4 GPIOPATH "gpio4/value"
+#define GPIO5 GPIOPATH "gpio5/value"
 
 int main(void){
-  子プロセスから親プロセスへの通信
-int pipefd[2];
-if (pipe(pipefd) < 0) {
-  perror("pipe");
-  exit(-1);
-}
-pid_t pid = fork();
-if (pid < 0) {
-  perror("fork");
-  exit(-1);
-} else if (pid == 0) {
-  // 子プロセス
-  close(pipefd[1]); //書き込みをクローズ
 
-  char buf[128];
-  read(pipefd[0], buf, sizeof buf);
-  printf("child=[%s]\n", buf);
+  int pipefd[2];
+  if (pipe(pipefd) < 0) {
+    perror("pipe");
+    exit(-1);
+  }
 
-  close(pipefd[0]);
-  return 0;
-} else {
-  // 親プロセス
-  close(pipefd[0]); //読み込みをクローズ
+  pid_t pid = fork();
 
-  char *s = "send from parent";
-  write(pipefd[1], s, strlen(s));
+  if (pid < 0) {
 
+    perror("fork");
+    exit(-1);
 
-  close(pipefd[1]);
-  return 0;
-}
+  } else if (pid == 0) { // 子プロセス
+
+    close(pipefd[1]); //書き込みをクローズ
+    int buf;
+    read(pipefd[0], buf, sizeof buf);
+    printf("child=[%d]\n", buf);
+    close(pipefd[0]);
+
+    return 0;
+
+  } else {
+    // 親プロセス
+    close(pipefd[0]); //読み込みをクローズ
+    int *s = 1;
+    write(pipefd[1], s, 1);
+    close(pipefd[1]);
+    return 0;
+  }
+
 }
