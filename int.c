52ac6be5f312bfe69c9de0e9f8093376e3d35826
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-11-07 19:42:46 +0900

    fix

--- int.c
+++ int.c
@@ -4,39 +4,35 @@
 #include <unistd.h>
 
 int main(void){
+  子プロセスから親プロセスへの通信
+int pipefd[2];
+if (pipe(pipefd) < 0) {
+  perror("pipe");
+  exit(-1);
+}
+pid_t pid = fork();
+if (pid < 0) {
+  perror("fork");
+  exit(-1);
+} else if (pid == 0) {
+  // 子プロセス
+  close(pipefd[1]); //書き込みをクローズ
 
-  int pipefd[2]; // パイプ用ファイルディスクリプタ
+  char buf[128];
+  read(pipefd[0], buf, sizeof buf);
+  printf("child=[%s]\n", buf);
 
-  if (pipe(pipefd) < 0) {
-    perror("pipe");
-    exit(-1);
-  }
+  close(pipefd[0]);
+  return 0;
+} else {
+  // 親プロセス
+  close(pipefd[0]); //読み込みをクローズ
 
-  pid_t pid = fork(); // int(typedef int pid_t)
+  char *s = "send from parent";
+  write(pipefd[1], s, strlen(s));
 
-  if (pid < 0) { //エラー処理
 
-    perror("fork");
-    exit(-1);
-
-  } else if (pid == 0) { // ( == 子プロセス )
-
-    close(pipefd[1]); // 書き込みを閉じる
-    int buf;
-    read(pipefd[0], buf, sizeof buf);
-    printf("parent=[%d]\n", buf);
-
-    close(pipefd[0]);
-    return 0;
-
-  } else {
-
-    close(pipefd[0]); // 読み込みを閉じる
-    int *buf = 1;
-    write(pipefd[1], buf, strlen(buf));
-
-    close(pipefd[1]);
-    return 0;
-
-  }
+  close(pipefd[1]);
+  return 0;
+}
 }
