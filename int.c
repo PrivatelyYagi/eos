60fb34aebe48c34418068505277352a91d8695d5
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-11-07 18:28:10 +0900

    test intr

--- int.c
+++ int.c
@@ -1,67 +1,51 @@
+#include <fcntl.h>
 #include <stdio.h>
-#include <sys/time.h>
-#include <wiringPi.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <poll.h>
 
-// Which GPIO pin we're using
-#define PIN 5
-// How much time a change must be since the last in order to count as a change
-#define IGNORE_CHANGE_BELOW_USEC 10000
+int main(){
+int i;
+int fd;
+int ret;
+struct pollfd pfd;
+char c;
 
-// Current state of the pin
-static volatile int state;
-// Time of last change
-struct timeval last_change;
+//Enable gpio25
+fd = open(“/sys/class/gpio/export”, O_WRONLY);
+write(fd, “25”, 2);
+close(fd);
 
-// Handler for interrupt
-void handle(void) {
-  struct timeval now;
-  unsigned long diff;
+//Set gpio25 as input
+fd = open(“/sys/class/gpio/gpio25/direction”, O_WRONLY);
+write(fd, “in”, 2);
+close(fd);
 
-  gettimeofday(&now, NULL);
+//Set gpio25 interrupt
+fd = open(“/sys/class/gpio/gpio25/edge”, O_WRONLY);
+//write(fd, “falling”, 7);
+write(fd, “both”, 4);
+close(fd);
 
-  // Time difference in usec
-  diff = (now.tv_sec * 1000000 + now.tv_usec) - (last_change.tv_sec * 1000000 + last_change.tv_usec);
-
-  // Filter jitter
-  if (diff > IGNORE_CHANGE_BELOW_USEC) {
-    if (state) {
-      printf("Falling\n");
-    }
-    else {
-      printf("Rising\n");
-    }
-
-    state = !state;
-  }
-
-  last_change = now;
+//Wait for interrupt, repeat 10 times.
+fd = open(“/sys/class/gpio/gpio25/value”, O_RDONLY);
+pfd.fd = fd;
+pfd.events = POLLPRI;
+for(i = 0; i < 10; i++){
+lseek(fd, 0, SEEK_SET);
+ret = poll(&pfd, 1, 3000);
+read(fd, &c, 1);
+if(ret == 0)
+printf(“Timeout\n”);
+else
+printf(“%d=%c\n”, i, c);
 }
+close(fd);
 
-int main(void) {
-  // Init
-  wiringPiSetup();
+//Disable gpio25
+fd = open(“/sys/class/gpio/unexport”, O_WRONLY);
+write(fd, “25”, 2);
+close(fd);
 
-  // Set pin to output in case it's not
-  pinMode(PIN, OUTPUT);
-
-  // Time now
-  gettimeofday(&last_change, NULL);
-
-  // Bind to interrupt
-  wiringPiISR(PIN, INT_EDGE_BOTH, &handle);
-
-  // Get initial state of pin
-  state = digitalRead(PIN);
-
-  if (state) {
-    printf("Started! Initial state is on\n");
-  }
-  else {
-    printf("Started! Initial state is off\n");
-  }
-
-  // Waste time but not CPU
-  for (;;) {
-    sleep(1);
-  }
+return(0);
 }
