e0391e81191c84bf32be2b48ea8a6c5662806360
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-11-08 11:51:39 +0900

    test poll

--- rep01_01.c
+++ rep01_01.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include<fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <poll.h>
 
 #define GPIOPATH "/sys/class/gpio/"
 #define GPIO2 GPIOPATH "gpio2/value"
@@ -9,14 +13,20 @@
 #define GPIO4 GPIOPATH "gpio4/value"
 #define GPIO5 GPIOPATH "gpio5/value"
 
+#define TIMEOUT_MS (3000) // 3秒 #define INBUF_SIZE (80)
+#define PFD_SIZE (1)
+
 int main(void){
 
   int fdgpio2,fdgpio3,fdgpio4,fdgpio5;
+  int i, ready, len;
   struct timespec ts;
-  int i;
+  struct pollfd pfd[PFD_SIZE];
 
   ts.tv_sec = 0; //sec.
   ts.tv_nsec = 500000000; //nano sec. (500ms)
+
+  system("bash all.sh 2 3 4 5"); // GPIOの設定
 
   fdgpio2 = open(GPIO2 , O_WRONLY | O_SYNC);
   if(fdgpio2 < 0){
@@ -36,11 +46,51 @@ int main(void){
     exit(EXIT_FAILURE);
   }
 
-  fdgpio5 = open(GPIO5 , O_WRONLY | O_SYNC);
+  fdgpio5 = open(GPIO5, O_RDWR);
   if(fdgpio5 < 0){
     perror("cannot open " GPIO5 );
     exit(EXIT_FAILURE);
   }
 
+  pfd[0].fd = fdgpio5;
+  pfd[0].events = POLLPRI | POLLERR;
 
+  write(STDOUT_FILENO, "waiting for interrupt...¥n", strlen("waiting for interrupt...¥n"));
+
+  for(;;){
+    pret=poll(pfd, PFD_SIZE, TIMEOUT_MS);
+    showPollRevents(STDOUT_FILENO, pfd[0].revents);
+    if(pret==0){
+      write(STDOUT_FILENO, "timeout.¥n", strlen("timeout.¥n"));
+    }else{
+      write(STDOUT_FILENO, "sw¥n", strlen("sw¥n"));
+      lseek(fd, 0, SEEK_SET); len=read(fd, inbuf, INBUF_SIZE);
+      write(STDOUT_FILENO, inbuf, len);
+    }
+  }
+
+  system("bash end.sh 2 3 4 5");  // GPIOを開放
+
+  return (EXIT_SUCCESS);
+}
+
+void showPollRevents(int fd, short revents) {
+  if(revents & POLLIN){
+    write(fd, "POLLIN¥n", strlen("POLLIN¥n"));
+  }
+  if(revents & POLLPRI){
+    write(fd, "POLLPRI¥n", strlen("POLLPRI¥n"));
+  }
+  if(revents & POLLOUT){
+    write(fd, "POLLOUT¥n", strlen("POLLOUT¥n"));
+  }
+  if(revents & POLLERR){
+    write(fd, "POLLERR¥n", strlen("POLLERR¥n"));
+  }
+  if(revents & POLLHUP){
+    write(fd, "POLLHUP¥n", strlen("POLLHUP¥n"));
+  }
+  if(revents & POLLNVAL){
+    write(fd, "POLLNVAL¥n", strlen("POLLNVAL¥n"));
+  }
 }
