6a2db6d6040cfe51b53b637a2bed6c1b62a605c7
Shinsaku Yagi <jaming.jaming.g@gmail.com>
2017-11-08 12:06:28 +0900

    fix shell (add edge setteing)

--- all.sh
+++ all.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+GPIOPATH="/sys/class/gpio"
+
 if [ $# -ne 4 ]; then
   echo "指定された引数は$#個です。" 1>&2
   exit 1
 fi
 
-echo $1 > /sys/class/gpio/export
-echo 'out' > /sys/class/gpio/gpio$1/direction
-echo $2 > /sys/class/gpio/export
-echo 'out' > /sys/class/gpio/gpio$2/direction
-echo $3 > /sys/class/gpio/export
-echo 'out' > /sys/class/gpio/gpio$3/direction
-echo $4 > /sys/class/gpio/export
-echo 'in' > /sys/class/gpio/gpio$4/direction
-
+echo $1 > ${GPIOPATH}/export
+echo 'out' > ${GPIOPATH}/gpio$1/direction
+echo $2 > ${GPIOPATH}/export
+echo 'out' > ${GPIOPATH}/gpio$2/direction
+echo $3 > ${GPIOPATH}/export
+echo 'out' > ${GPIOPATH}/gpio$3/direction
+echo $4 > ${GPIOPATH}/export
+echo 'in' > ${GPIOPATH}/gpio$4/direction
+echo 'both' > ${GPIOPATH}/gpio5/edge
 exit 0
